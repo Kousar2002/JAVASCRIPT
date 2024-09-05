320a713321bec716df21a24a02eba5b1f953afad
Kousar2002 <mullakousarbanu772@gmail.com>
2024-09-05 21:55:00 +0530

    msg

---
+++ day2-Variables/demo.js
@@ -0,0 +1,124 @@
+// let d;
+// console.log(typeof(d));
+
+// let first_name=null;
+// console.log(typeof(first_name));
+
+// let last_name="kousar";
+// console.log(typeof(last_name));
+
+// let user_age=21;
+// console.log(typeof(user_age));
+
+// last_name=true;
+// console.log(typeof(last_name));
+
+// var a=true;
+// console.log(typeof(a));
+
+// var e=null;
+// console.log(typeof(e));
+
+// var f=undefined;
+// console.log(typeof(f));
+
+// var b={
+//     id:4,
+//     age:30,
+
+// };
+// console.log(b.id);
+// console.log(b.age);
+
+// var const=200;
+// var let=300;
+// var d=function(){
+//     console.log("helloworld");
+// }
+// d();
+
+// var c=5;
+// let g=function(){
+//     var a=5;
+//     console.log(c+a);
+// }
+// g()
+
+// let a=5;
+// let b=function(g){
+//     console.log(g);
+// }
+// b(a);
+// let arr=[1.34,"apple",5.6]
+// console.log(typeof(arr));
+// var b={
+//        id:4,
+//        age:30,
+    
+// };
+// console.log(typeof(b));
+
+// var c=new Date();
+// console.log(c);
+// console.log(typeof(c));
+// let g=function(){
+//     // var a=5;
+//     console.log("hello");
+// }
+// g()
+// console.log(typeof(g));
+// z=4;
+// console.log(typeof(z));
+// const value1=Symbol('hello');
+// const value2=Symbol('hello');
+// console.log(value1!=value2);
+// const x=Symbol();
+// console.log(typeof(x));
+// const x=Symbol('hey');
+// console.log(x);
+
+// const value1=Symbol('hello');
+// const value2=Symbol('hello');
+// console.log(value1)
+// console.log(value1!=value2);
+// console.log(value1.description);
+
+// let id=Symbol("id");
+// let person={
+//     name:"jack",
+//     [id]:123
+// };
+// console.log(person);
+
+// let id=Symbol("id");
+// let person={
+//     name:"jack",
+//     age:25,
+//     [id]:12
+// }
+// for(let key in person){
+//     console.log(key);
+// }
+
+// let person={
+//     name:"jack"
+// };
+// let id=Symbol("id");
+// person[id]=12;
+// console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
